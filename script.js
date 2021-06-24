8dbdaf7ecc015607a3e47396ca193e47dc839916
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-06-24 13:51:55 +0100

    added js functions 1

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-$('.add-more').click(function() {
-    $('.add').clone().appendTo('.add_dynamic');
-    $('.add_dynamic .add').addClass('single remove');
-    $('.single .add-more').remove();
-    $('.single').append('<a href="#" class="remove-field btn-remove-customer">Remove Fields</a>');
-    $('.add_dynamic > .single').attr("class", "remove");
-  
-    $('.add_dynamic input').each(function() {
-      var count = 0;
-      var fieldname = $(this).attr("name");
-      $(this).attr('name', fieldname + count);
-      count++;
-    });
-  
-  });
-  
-  $(document).on('click', '.remove-field', function(e) {
-    $(this).parent('.remove').remove();
+$('.addmore').click(function(e) {
     e.preventDefault();
-  });
+   $('.table-data').fadeIn();
+   let cat = $('select[name=Utilizaton_Category]').val();
+   let doze = $('select[name=doze]').val();
+   let imu = $('input[name=imunization]').val();
+   let qty = $('input[name=quantity]').val();
+   let tarif = $('input[name=tariff]').val();
+   $('.table-data tbody').append('<tr><td>'+cat+'</td><td>'+doze+'</td><td>'+imu+'</td><td>'+qty+'</td><td>'+tarif+'</td><td><button class="btn btn-close btn-danger remove-item"></button></td></tr>')
+   
+   $('select[name=Utilizaton_Category]').val('');
+   $('select[name=doze]').val('');
+   $('input[name=imunization]').val('');
+   $('input[name=quantity]').val('');
+   $('input[name=tariff]').val('');
+});
+  
+  $(document).on('click', '.remove-item', function(e) {
+    e.preventDefault();
+    $(this).closest('tr').remove()
+});
+
