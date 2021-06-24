80054a86202f33cecd2e79b294011a7d6e1e7dca
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-06-24 14:03:46 +0100

    added few changes and deleted addmore.html fies

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-$('.addmore').click(function(e) {
-    e.preventDefault();
-   $('.table-data').fadeIn();
-   let cat = $('select[name=Utilizaton_Category]').val();
-   let doze = $('select[name=doze]').val();
-   let imu = $('input[name=imunization]').val();
-   let qty = $('input[name=quantity]').val();
-   let tarif = $('input[name=tariff]').val();
-   $('.table-data tbody').append('<tr><td>'+cat+'</td><td>'+doze+'</td><td>'+imu+'</td><td>'+qty+'</td><td>'+tarif+'</td><td><button class="btn btn-close btn-danger remove-item"></button></td></tr>')
-   
-   $('select[name=Utilizaton_Category]').val('');
-   $('select[name=doze]').val('');
-   $('input[name=imunization]').val('');
-   $('input[name=quantity]').val('');
-   $('input[name=tariff]').val('');
-});
-  
-  $(document).on('click', '.remove-item', function(e) {
-    e.preventDefault();
-    $(this).closest('tr').remove()
+//Daignosis
+$('.add-input').click(function(e) {
+  e.preventDefault();
+ $('.table-data').fadeIn();
+ let cat = $('select[name=Utilizaton_Category]').val();
+ let doze = $('select[name=doze]').val();
+ let imu = $('input[name=imunization]').val();
+ let qty = $('input[name=quantity]').val();
+ let tarif = $('input[name=tariff]').val();
+ $('.table-data tbody').append('<tr><td>'+cat+'</td><td>'+doze+'</td><td>'+imu+'</td><td>'+qty+'</td><td>'+tarif+'</td><td><button class="btn btn-close btn-danger remove-item"></button></td></tr>')
+ 
+ $('select[name=Utilizaton_Category]').val('');
+ $('select[name=doze]').val('');
+ $('input[name=imunization]').val('');
+ $('input[name=quantity]').val('');
+ $('input[name=tariff]').val('');
 });
 
+$(document).on('click', '.remove-item', function(e) {
+  e.preventDefault();
+  $(this).closest('tr').remove()
+});
+
+
+// Utilization
+$('.addmore').click(function(e) {
+  e.preventDefault();
+ $('.table-data').fadeIn();
+ let cat = $('select[name=Utilizaton_Category]').val();
+ let doze = $('select[name=doze]').val();
+ let imu = $('input[name=imunization]').val();
+ let qty = $('input[name=quantity]').val();
+ let tarif = $('input[name=tariff]').val();
+ $('.table-data tbody').append('<tr><td>'+cat+'</td><td>'+doze+'</td><td>'+imu+'</td><td>'+qty+'</td><td>'+tarif+'</td><td><button class="btn btn-close btn-danger remove-item"></button></td></tr>')
+ 
+ $('select[name=Utilizaton_Category]').val('');
+ $('select[name=doze]').val('');
+ $('input[name=imunization]').val('');
+ $('input[name=quantity]').val('');
+ $('input[name=tariff]').val('');
+});
+
+$(document).on('click', '.remove-item', function(e) {
+  e.preventDefault();
+  $(this).closest('tr').remove()
+});
