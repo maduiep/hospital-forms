ed002e0632549015a529eb85c2a7e723cfbaea85
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-06-25 16:03:37 +0100

    updated html and css files

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 //  let qty = $('input[name=quantity]').val();
 //  let tarif = $('input[name=tariff]').val();
 //  $('.table-data tbody').append('<tr><td>'+cat+'</td><td>'+doze+'</td><td>'+imu+'</td><td>'+qty+'</td><td>'+tarif+'</td><td><button class="btn btn-close btn-danger remove-item"></button></td></tr>')
- 
+
 //  $('select[name=Utilizaton_Category]').val('');
 //  $('select[name=doze]').val('');
 //  $('input[name=imunization]').val('');
@@ -24,97 +24,92 @@
 
 // Utilization
 $(document).ready(function () {
-  $('.addmore').click(function(e){
+  $('.addmore').click(function (e) {
     e.preventDefault();
-      $('.utilize-data').append('<br><label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Utilizaton Category</label>'+
-      '<div class="form-floating">'+
-      '<select class="form-select" name="Utilizaton_Category" id="floatingSelectGrid" aria-label="Floating label select example">'+
-      '<option selected></option>'+
-      '<option value="1">1</option>'+
-      '<option value="2">2</option>'+
-      '<option value="3">3</option>'+
-      '</select>'+
-      '<label for="floatingSelectGrid" style="padding-top: 1.6rem;">Immunization</label>'+
-      '</div>'+
-      '</div>'+
-      '<div class="col-md">'+
-      '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Doze</label>'+
-      '<div class="form-floating">'+
-      '<select class="form-select" name="doze" id="floatingSelectGrid" aria-label="Floating label select example">'+
-      '<option selected></option>'+
-      '<option value="1">1</option>'+
-      '<option value="2">2</option>'+
-      '<option value="3">3</option>'+
-      '</select>'+
-      '<label for="floatingSelectGrid" style="padding-top: 1.6rem;">Select Doze</label>'+
-      '</div>'+
-      '</div>'+
-      '</div>'+
-      '<div class="row g-2">'+
-      '<div class="col-md">'+
-      '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Immunization</label>'+
-      '<div class="form-floating">'+
-      '<input type="text" name="imunization" class="form-control" id="floatingInputGrid">'+
-      '<label for="floatingInputGrid" style="padding-top: 1.5rem">'+
-      'Input Immunization</label>'+
-          '</div>'+
-          '</div>'+
-          '<div class="col-md">'+
-          '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Quantity</label>'+
-          '<div class="form-floating">'+
-          '<input type="number" name="quantity" class="form-control" id="floatingInputGrid">'+
-          '<label for="floatingInputGrid" style="padding-top: 1.5rem">'+
-          '0.00</label>'+
-          '</div>'+
-          '</div>'+
-          '</div>'+
-          '<div class="row g-2">'+
-          '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Tariff Fee</label>'+
-          '<div class="form-floating" style="width: 42.5% !important;">'+
-          '<input type="number" name="tariff" class="form-control" id="floatingInputGrid">'+
-          '<label for="floatingInputGrid" style="padding-top: 1.5rem">'+
-          '0.00</label>'+
-      '</div>'+
-  '</div>'+
-          '<button class="btn btn-close btn-danger remove-item">Remove</button>'+
-          '<br><br><br>');
-    });
+    $('.utilize-data').append('<div class="row g-2">' + '<div class="col-md">' +
+      '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Utilizaton Category</label>' +
+      '<div class="form-floating">' +
+      '<select class="form-select" name="Utilizaton_Category" id="floatingSelectGrid" aria-label="Floating label select example">' +
+      '<option selected></option>' +
+      '<option value="1">1</option>' +
+      '<option value="2">2</option>' +
+      '<option value="3">3</option>' +
+      '</select>' +
+      '<label for="floatingSelectGrid" style="padding-top: 1.6rem;">Immunization</label>' +
+      '</div>' +
+      '</div>' +
+      '<div class="col-md">' +
+      '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Doze</label>' +
+      '<div class="form-floating">' +
+      '<select class="form-select" name="doze" id="floatingSelectGrid" aria-label="Floating label select example">' +
+      '<option selected></option>' +
+      '<option value="1">1</option>' +
+      '<option value="2">2</option>' +
+      '<option value="3">3</option>' +
+      '</select>' +
+      '<label for="floatingSelectGrid" style="padding-top: 1.6rem;">Select Doze</label>' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
+
+      '<button class="btn btn-close btn-danger remove-item">Remove</button>' +
+      '<br><br><br>');
+  });
 })
 
-$(document).on('click', '.remove-item', function(e) {
+$(document).on('click', '.remove-item', function (e) {
   e.preventDefault();
   $(this).closest('tr').remove()
 });
 
 $(document).ready(function () {
-  $('.add-input').click(function(e){
+  $('.add-input').click(function (e) {
     e.preventDefault();
-      $('.diagnosis-data').append('<br><label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Diagnosis Code</label>'+
-      '<div class="form-floating">'+
-          '<select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">'+
-            '<option selected></option>'+
-            '<option value="1"></option>'+
-            '<option value="2"></option>'+
-            '<option value="3"></option>'+
-          '</select>'+
-          '<label for="floatingSelectGrid" style="padding-top: 1.6rem;">Input code</label>'+
-      '</div>'+
-  '</div>'+
-  '<div class="col-md">'+
-      '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Diagnosis Description</label>'+
-      '<div class="form-floating">'+
-          '<select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">'+
-            '<option selected></option>'+
-            '<option value="1"></option>'+
-            '<option value="2"></option>'+
-            '<option value="3"></option>'+
-          '</select>'+
-          '<label for="floatingSelectGrid" style="padding-top: 1.6rem;">Select Description</label>'+
-          '<button class="btn btn-close btn-danger remove-item">Remove</button>'+
-          '<br><br><br>');
-    });
+    $('.diagnosis-data').append('<div class="row g-2">' + '<div class="col-md">' + '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Diagnosis Code</label>' +
+      '<div class="form-floating">' +
+      '<select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">' +
+      '<option selected></option>' +
+      '<option value="1"></option>' +
+      '<option value="2"></option>' +
+      '<option value="3"></option>' +
+      '</select>' +
+      '<label for="floatingSelectGrid" style="padding-top: 1.6rem;">Input code</label>' +
+      '</div>' +
+      '</div>' +
+      '<div class="col-md">' +
+      '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Diagnosis Description</label>' +
+      '<div class="form-floating">' +
+      '<select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">' +
+      '<option selected></option>' +
+      '<option value="1"></option>' +
+      '<option value="2"></option>' +
+      '<option value="3"></option>' +
+      '</select>' +
+      '<label for="floatingSelectGrid" style="padding-top: 1.6rem;">Select Description</label>' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
+      '<div class="row g-2">' +
+                '<div class="col-md">' +
+                    '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Presenting Complaint</label>' +
+                    '<div class="form-floating">' +
+                        '<input type="email" class="form-control" id="floatingInputGrid">' +
+                        '<label for="floatingInputGrid" style="padding-top: 1.5rem"> Input Complaint</label>'+
+                    '</div>' +
+                '</div>' +
+                '<div class="col-md">' +
+                    '<label for="diagnosis" class="form-label" style="padding-top: 1rem; padding-left: 2rem; color: rgb(129, 129, 199);">Date</label>'+
+                    '<div class="form-floating">' +
+                        '<input type="email" class="form-control" id="floatingInputGrid">'+
+                        '<label for="floatingInputGrid" style="padding-top: 1.5rem">Select Date</label>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+      '<button class="btn btn-close btn-danger remove-item">Remove</button>' +
+      '<br><br><br>');
+  });
 })
-$(document).on('click', '.remove-item', function(e) {
+$(document).on('click', '.remove-item', function (e) {
   e.preventDefault();
   $(this).closest('diagnosis-data').remove()
 });
